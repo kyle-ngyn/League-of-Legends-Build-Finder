3206d261923aac6b0594e2ffe9a24ad459e48cb4
Kyle Nguyen <kylelikesdonuts@gmail.com>
2024-05-28 05:13:51 -0700

    Reduced execution time from 10s -> 5s
    Now using Playwright instead of Selenium!

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
+import re
+import time
 import webbrowser
 import urllib.parse
-import re
 import tkinter as tk
 from tkinter import ttk
-from bs4 import BeautifulSoup
 from player import Player
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from playwright.sync_api import sync_playwright
 
 def CenterWindowToDisplay(Screen: tk, width: int, height: int):
     screen_width = Screen.winfo_screenwidth()
@@ -29,88 +25,86 @@ def sorting_key(player):
     )
 
 def ok_button_click(event=None):
+    start_time = time.time()
     region = combo_box.get()
     champion = champion_entry.get()
+    root.destroy()
     url = "https://www.op.gg/leaderboards/champions/" + champion.lower() + "?region=" + region.lower()
 
-    # Headless browsers are slow and performance-intensive, but lift all restrictions on web scraping.
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
-    driver.maximize_window()
-    driver.get(url)
-    # Since this is a dynamically changing page, we must wait for some elements to appear.
-    try:
-        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, "//div[@class='css-12ijbdy e1swkqyq0']")))
-    except Exception as e:
-        print("Error waiting for elements:", e)
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
 
-    # Write HTML code to a text file for debugging.
-    #with open("webpage.html", "w", encoding="utf-8") as file:
-    #    file.write(driver.page_source)
+        # Since this is a dynamically changing page, we must wait for some elements to appear.
+        # Tags for ranks, winrates, and games played for the top 3 players are different from the rest of the players.
+        page.wait_for_selector("span.css-ao94tw.e1swkqyq1")
+        page.wait_for_selector("span.css-1mbuqon.e1swkqyq2")
+        page.wait_for_selector("small")
+        page.wait_for_selector("td.css-13jn5d5.e1nwpw1h3")
+        page.wait_for_selector("span > em")
+        page.wait_for_selector("div.text")
+        page.wait_for_selector("span > em > b")
+        page.wait_for_selector("td.css-1amolq6.eyczova1 > b")
 
-    # Tags for ranks, winrates, and games played for the top 3 players are different from the rest of the players.
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    player_names = soup.find_all("span", class_="css-ao94tw e1swkqyq1")[:5]
-    player_tags = soup.find_all("span", class_="css-1mbuqon e1swkqyq2")[:5]
-    player_ranks = soup.find_all("small")[:3]
-    player_ranks += soup.find_all("td", class_="css-13jn5d5 e1nwpw1h3")[:2]
-    player_winrate = soup.select("span > em")[:6]
-    player_winrate += soup.find_all("div", class_="text")[:2]
-    player_games_played = soup.select("span > em > b")
-    player_games_played += soup.select("td.css-1amolq6.eyczova1 > b")[:2]
+        # Write HTML code to a text file for debugging.
+        # with open("webpage.html", "w", encoding="utf-8") as file:
+        #     file.write(page.content())
 
-    # Temporary lists to hold player data.
-    names = [name.get_text() for name in player_names]
-    tags = [tag.get_text().replace('#', '') for tag in player_tags]
-    ranks = []
-    for rank in player_ranks:
-        rank_text = rank.get_text().split("Lv.")[0].strip().lower()
-        if "challenger" in rank_text:
-            rank_text = "Challenger"
-        elif "grandmaster" in rank_text:
-            rank_text = "Grandmaster"
-        elif "master" in rank_text:
-            rank_text = "Master"
-        elif "diamond 1" in rank_text:
-            rank_text = "Diamond 1"
-        elif "diamond 2" in rank_text:
-            rank_text = "Diamond 2"
-        ranks.append(rank_text)
-    winrates = []
-    for winrate in player_winrate:
-        winrate_text = winrate.get_text().strip()
-        match = re.search(r'\b\d+%$', winrate_text)
-        if match:
-            winrate_percentage = match.group()
-            winrates.append(winrate_percentage)
-    games_played = [number.get_text() for number in player_games_played]
+        # Temporary lists to hold player data.
+        player_names = page.query_selector_all("span.css-ao94tw.e1swkqyq1")[:5]
+        player_tags = page.query_selector_all("span.css-1mbuqon.e1swkqyq2")[:5]
+        player_ranks = page.query_selector_all("small")[:3]
+        player_ranks += page.query_selector_all("td.css-13jn5d5.e1nwpw1h3")[:2]
+        player_winrate = page.query_selector_all("span > em")[:6]
+        player_winrate += page.query_selector_all("div.text")[:2]
+        player_games_played = page.query_selector_all("span > em > b")
+        player_games_played += page.query_selector_all("td.css-1amolq6.eyczova1 > b")[:2]
 
-    # Create Player objects.
-    players = []
-    for name, tag, rank, winrate, games in zip(names, tags, ranks, winrates, games_played):
-        player = Player(name, tag, rank, winrate, games)
-        players.append(player)
-    #for player in players:
-    #    player.showDescription()
+        names = [name.text_content() for name in player_names]
+        tags = [tag.text_content().replace('#', '') for tag in player_tags]
+        ranks = []
+        for rank in player_ranks:
+            rank_text = rank.text_content().split("Lv.")[0].strip().lower()
+            if "challenger" in rank_text:
+                rank_text = "Challenger"
+            elif "grandmaster" in rank_text:
+                rank_text = "Grandmaster"
+            elif "master" in rank_text:
+                rank_text = "Master"
+            elif "diamond 1" in rank_text:
+                rank_text = "Diamond 1"
+            elif "diamond 2" in rank_text:
+                rank_text = "Diamond 2"
+            ranks.append(rank_text)
+        winrates = []
+        for winrate in player_winrate:
+            winrate_text = winrate.text_content().strip()
+            match = re.search(r'\b\d+%$', winrate_text)
+            if match:
+                winrate_percentage = match.group()
+                winrates.append(winrate_percentage)
+        games_played = [number.text_content() for number in player_games_played]
 
-    # Open the OP.GG webpage for the "best" player.
-    players.sort(key=sorting_key)
-    best_player = players[0]
-    url = "https://www.op.gg/summoners/" + region.lower() + "/" + urllib.parse.quote(best_player.name) + "-" + urllib.parse.quote(best_player.tag)
-    webbrowser.open(url, new=2, autoraise=False)
+        # Create Player objects.
+        players = []
+        for name, tag, rank, winrate, games in zip(names, tags, ranks, winrates, games_played):
+            player = Player(name, tag, rank, winrate, games)
+            players.append(player)
+        # for player in players:
+        #     player.showDescription()
 
-    '''
-    driver.get(url)
-    search_box = driver.find_element_by_xpath("//div[@class='search']//input[@id='championInput']")
-    search_box.send_keys("Malphite")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='champion-name' and text()='Malphite']")))
-    malphite_button = driver.find_element_by_xpath("//span[@class='champion-name' and text()='Malphite']")
-    malphite_button.click()
-    '''
+        # Open the OP.GG webpage for the "best" player.
+        players.sort(key=sorting_key)
+        best_player = players[0]
+        player_url = "https://www.op.gg/summoners/" + region.lower() + "/" + urllib.parse.quote(best_player.name) + "-" + urllib.parse.quote(best_player.tag)
+        webbrowser.open(player_url, new=2, autoraise=False)
+        browser.close()
 
-    driver.quit()
-    root.destroy()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print("Execution Time:", execution_time, "seconds")
+
 
 root = tk.Tk()
 root.geometry(CenterWindowToDisplay(root, 570, 130))
