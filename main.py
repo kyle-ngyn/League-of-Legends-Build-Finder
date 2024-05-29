238d23647f35df002fed2afb5e989c205779689e
Kyle Nguyen <kylelikesdonuts@gmail.com>
2024-05-28 21:07:30 -0700

    Cleaning up main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import webbrowser
 import urllib.parse
 import tkinter as tk
 from tkinter import ttk
-from player import Player
 from playwright.sync_api import sync_playwright
 
 def CenterWindowToDisplay(Screen: tk, width: int, height: int):
@@ -14,13 +13,21 @@ def CenterWindowToDisplay(Screen: tk, width: int, height: int):
     y = int((screen_height / 2) - (height / 1.5))
     return f"{width}x{height}+{x}+{y}"
 
+class Player:
+    def __init__(self, name, tag, rank, winrate, games_played):
+        self.name = name
+        self.tag = tag
+        self.rank = rank
+        self.winrate = winrate
+        self.games_played = games_played
+
 # The "best" player is the one with the highest rank. If ranks are equal, choose the one with a higher winrate.
 # If winrates are equal, choose the one with the most games played.
 def sorting_key(player):
     rank_order = ["Challenger", "Grandmaster", "Master", "Diamond 1", "Diamond 2"]
     return (
         rank_order.index(player.rank),
-        -float(player.winrate.strip('%')),
+        -float(player.winrate.strip("%")),
         -int(player.games_played)
     )
 
@@ -29,7 +36,7 @@ def ok_button_click(event=None):
     region = combo_box.get()
     champion = champion_entry.get()
     root.destroy()
-    url = "https://www.op.gg/leaderboards/champions/" + champion.lower() + "?region=" + region.lower()
+    url = f"https://www.op.gg/leaderboards/champions/{champion.lower()}?region={region.lower()}"
 
     with sync_playwright() as p:
         browser = p.firefox.launch(headless=True)
@@ -38,18 +45,7 @@ def ok_button_click(event=None):
 
         # Since this is a dynamically changing page, we must wait for some elements to appear.
         # Tags for ranks, winrates, and games played for the top 3 players are different from the rest of the players.
-        page.wait_for_selector("span.css-ao94tw.e1swkqyq1")
-        page.wait_for_selector("span.css-1mbuqon.e1swkqyq2")
-        page.wait_for_selector("small")
-        page.wait_for_selector("td.css-13jn5d5.e1nwpw1h3")
-        page.wait_for_selector("span > em")
-        page.wait_for_selector("div.text")
-        page.wait_for_selector("span > em > b")
-        page.wait_for_selector("td.css-1amolq6.eyczova1 > b")
-
-        # Write HTML code to a text file for debugging.
-        # with open("webpage.html", "w", encoding="utf-8") as file:
-        #     file.write(page.content())
+        page.wait_for_selector("span.css-ao94tw.e1swkqyq1, span.css-1mbuqon.e1swkqyq2, small, td.css-13jn5d5.e1nwpw1h3, span > em, div.text, span > em > b, td.css-1amolq6.eyczova1 > b")
 
         # Temporary lists to hold player data.
         player_names = page.query_selector_all("span.css-ao94tw.e1swkqyq1")[:5]
@@ -91,13 +87,11 @@ def ok_button_click(event=None):
         for name, tag, rank, winrate, games in zip(names, tags, ranks, winrates, games_played):
             player = Player(name, tag, rank, winrate, games)
             players.append(player)
-        # for player in players:
-        #     player.showDescription()
 
         # Open the OP.GG webpage for the "best" player.
         players.sort(key=sorting_key)
         best_player = players[0]
-        player_url = "https://www.op.gg/summoners/" + region.lower() + "/" + urllib.parse.quote(best_player.name) + "-" + urllib.parse.quote(best_player.tag)
+        player_url = f"https://www.op.gg/summoners/{region.lower()}/{urllib.parse.quote(best_player.name)}-{urllib.parse.quote(best_player.tag)}"
         webbrowser.open(player_url, new=2, autoraise=False)
         browser.close()
 
@@ -135,6 +129,6 @@ ok_button = tk.Button(root, text="OK", font=("Arial", 18), command=ok_button_cli
 ok_button.grid(row=1, column=1, sticky="e")
 
 # Enter key
-root.bind('<Return>', ok_button_click)
+root.bind("<Return>", ok_button_click)
 
 root.mainloop()
