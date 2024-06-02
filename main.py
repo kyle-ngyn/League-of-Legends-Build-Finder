341fa8c370aff885bc136c0d65c0150a5476ae77
Kyle Nguyen <kylelikesdonuts@gmail.com>
2024-06-01 22:20:00 -0700

    Removed webpage.html

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import webbrowser
 import urllib.parse
 import tkinter as tk
 from tkinter import ttk
+from collections import namedtuple
 from playwright.sync_api import sync_playwright
 
 def CenterWindowToDisplay(Screen: tk, width: int, height: int):
@@ -13,13 +14,7 @@ def CenterWindowToDisplay(Screen: tk, width: int, height: int):
     y = int((screen_height / 2) - (height / 1.5))
     return f"{width}x{height}+{x}+{y}"
 
-class Player:
-    def __init__(self, name, tag, rank, winrate, games_played):
-        self.name = name
-        self.tag = tag
-        self.rank = rank
-        self.winrate = winrate
-        self.games_played = games_played
+Player = namedtuple("Player", ["name", "tag", "rank", "winrate", "games_played"])
 
 # The "best" player is the one with the highest rank. If ranks are equal, choose the one with a higher winrate.
 # If winrates are equal, choose the one with the most games played.
@@ -45,7 +40,7 @@ def ok_button_click(event=None):
 
         # Since this is a dynamically changing page, we must wait for some elements to appear.
         # Tags for ranks, winrates, and games played for the top 3 players are different from the rest of the players.
-        page.wait_for_selector("span.css-ao94tw.e1swkqyq1, span.css-1mbuqon.e1swkqyq2, small, td.css-13jn5d5.e1nwpw1h3, span > em, div.text, span > em > b, td.css-1amolq6.eyczova1 > b")
+        page.wait_for_selector("div.css-12ijbdy.e1swkqyq0")
 
         # Temporary lists to hold player data.
         player_names = page.query_selector_all("span.css-ao94tw.e1swkqyq1")[:5]
@@ -58,7 +53,7 @@ def ok_button_click(event=None):
         player_games_played += page.query_selector_all("td.css-1amolq6.eyczova1 > b")[:2]
 
         names = [name.text_content() for name in player_names]
-        tags = [tag.text_content().replace('#', '') for tag in player_tags]
+        tags = [tag.text_content().replace("#", "") for tag in player_tags]
         ranks = []
         for rank in player_ranks:
             rank_text = rank.text_content().split("Lv.")[0].strip().lower()
@@ -76,17 +71,14 @@ def ok_button_click(event=None):
         winrates = []
         for winrate in player_winrate:
             winrate_text = winrate.text_content().strip()
-            match = re.search(r'\b\d+%$', winrate_text)
+            match = re.search(r"\b\d+%$", winrate_text)
             if match:
                 winrate_percentage = match.group()
                 winrates.append(winrate_percentage)
         games_played = [number.text_content() for number in player_games_played]
 
         # Create Player objects.
-        players = []
-        for name, tag, rank, winrate, games in zip(names, tags, ranks, winrates, games_played):
-            player = Player(name, tag, rank, winrate, games)
-            players.append(player)
+        players = [Player(name, tag, rank, winrate, games) for name, tag, rank, winrate, games in zip(names, tags, ranks, winrates, games_played)]
 
         # Open the OP.GG webpage for the "best" player.
         players.sort(key=sorting_key)
